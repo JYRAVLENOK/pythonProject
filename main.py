013167cf814b5f2ba2441b0e7a3cf0c1d6f46748
JYRAVLENOK <ivasiliev812@gmail.com>
2021-05-03 13:24:45 +0300

    first changes

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("1735384641:AAE_LoZ6D4AMDUe2_mxuWG7ajou4CnfKbY4", parse_mode=None)
+
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Что ты хочешь сделать?")
+    meeting = "Привет! <i>{0}</i> Что ты хочешь сделать?"
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    key_weather = types.KeyboardButton("Прогноз погоды")
+    key_value = types.KeyboardButton("Курс валюты")
+    markup.add(key_weather, key_value)
+    bot.send_message(message.chat.id, meeting.format(message.from_user.first_name),
+                     parse_mode="html", reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def answer_key(message):
+    if message.chat.type == "private":
+        if message.text == "Прогноз погоды":
+            pass
+        if message.text == "Курс валюты":
+            pass
+bot.polling()
